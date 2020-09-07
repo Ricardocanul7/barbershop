a502012927f3ed3cfb734297770940fb522c87c7
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-06 22:27:54 -0500

    Se agrega funcionalidad al boton de cerrar sesion, y se agrega ruta para mandar al login por medio de /home, /login y /admin

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,9 @@ Route::resource('/', 'LandingController');
 
 
 //pagina administrativa para dashboard del dueño
-Route::get('admin', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('dashboard', function(){
-    return view('admin.home.index');
+Route::get('admin', function(){
+    return redirect('login');
 });
